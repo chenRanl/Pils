74398f090ab5a4b1a355874b511a58e4e0b61720
andrearico <andi.rico@hotmail.com>
2020-11-11 20:00:31 -0400

    Changes in login page design and Login with onClick function

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView signUp;
+    private TextView Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +23,23 @@ public class MainActivity extends AppCompatActivity {
                 openActivity2();
             }
         });
+
+        Login = (TextView) findViewById(R.id.Login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
     }
 
     public void openActivity2(){
         Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void openActivity3(){
+        Intent intent = new Intent(this, MainActivity3.class);
         startActivity(intent);
     }
 }
