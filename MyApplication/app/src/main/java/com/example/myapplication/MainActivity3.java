bff7cec5a0eda19d609637ad77070d7c085b2485
andrearico <andi.rico@hotmail.com>
2020-11-12 03:49:01 -0400

    Error with onClick function solved

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity3.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class MainActivity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
         imageAbdos = (ImageView) findViewById(R.id.imageAbdos);
         imagePompes = (ImageView) findViewById(R.id.imagePompes);
         imageSuperman = (ImageView) findViewById(R.id.imageSuperman);
